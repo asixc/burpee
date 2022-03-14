ee00e602922a1c916a71805342feb6061b9acb62
Jotxee <jotxee@jotxee.dev>
2022-03-15 00:35:11 +0100

    first commit

---
+++ src/main/java/com/burpee/BurpeeApplication.java
@@ -0,0 +1,13 @@
+package com.burpee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurpeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurpeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/burpee/BurpeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burpee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurpeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
